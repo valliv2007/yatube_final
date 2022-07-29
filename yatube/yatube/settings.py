30df660e76b6db79fc849f2a6d09fc92e732af45
ILYA SNEZHKO <valliv2007@yandex.ru>
2022-07-29 14:19:01 +0300

    before 8 sprint

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -9,7 +9,9 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
                  '[::1]',
-                 'testserver', ]
+                 'testserver',
+                 'www.valliv.pythonanywhere.com',
+                 'valliv.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -22,7 +24,8 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
-    'sorl.thumbnail'
+    'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -33,6 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -109,3 +113,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}}
+
+INTERNAL_IPS = ['127.0.0.1']
